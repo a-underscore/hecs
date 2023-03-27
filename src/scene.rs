8b52860e298c459b1f5f9fe7e3cde1784c2f72fb
Brendan Bortz <brendansbortz@protonmail.com>
2023-03-27 16:49:48 -0400

    b

--- src/scene.rs
+++ src/scene.rs
@@ -4,3 +4,9 @@ pub struct Scene {
     pub display: Display,
     pub bg: [f32; 4],
 }
+
+impl Scene {
+    pub fn new(display: Display, bg: [f32; 4]) -> Self {
+        Self { display, bg }
+    }
+}
